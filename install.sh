07eb6ede16a6e2477480e60a02178a44865534ab
Toms Burgmanis <toms.burgmanis@gmail.com>
2022-11-11 10:47:49 +0100

    add tpm for tmux

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 PACKER_DIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 if [ ! -d $PACKER_DIR ]; then
+  echo "No packer, creating... ⏳\n"
   git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     $PACKER_DIR
+fi
+
+# Got to install tpm 🤌
+TPM_DIR=~/.tmux/plugins/tpm
+
+if [ ! -d $TPM_DIR ]; then
+  echo "No tpm, creating... ⏳\n"
+  git clone https://github.com/tmux-plugins/tpm\
+    $TPM_DIR
 fi
 
 for d in $(ls -d */ | cut -f1 -d '/');
