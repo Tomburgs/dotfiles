68b343c132c638bbc96894ae7a61f5eaea400823
Toms Burgmanis <toms.burgmanis@gmail.com>
2023-08-01 13:57:58 +0200

    feat: add rvm installation

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+DOTFILE_DIR=$(pwd)
+
 # Got to install packer 🤌
 PACKER_DIR=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
@@ -14,6 +16,16 @@ if [ ! -d $TPM_DIR ]; then
   echo "No tpm, creating... ⏳\n"
   git clone https://github.com/tmux-plugins/tpm\
     $TPM_DIR
+fi
+
+# Got to install rvm 🤙
+
+RVM_DIR=~/.rvm/src
+
+if [ ! -d $RVM_DIR ]; then
+  mkdir -p $RVM_DIR && cd $RVM_DIR && rm -rf ./rvm && \
+  git clone --depth 1 https://github.com/rvm/rvm.git && \
+  cd rvm && ./install && cd $DOTFILE_DIR
 fi
 
 for d in $(ls -d */ | cut -f1 -d '/');
